62905818761376b2dc19e46dcf6c8b685251954c
coding-to-music <connors.tom@gmail.com>
2022-05-09 05:34:19 +0000

    Build

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-import wiki from "wikijs";
-// const wiki = require('wikijs').default;
+// import wiki from "wikijs";
+const wiki = require("wikijs").default;
 
 wiki()
   .page("Batman")
   .then((page) => page.info("alterEgo"))
   .then(console.log); // Bruce Wayne
+
+// wiki()
+//   .page("Batman")
+//   .then((page) => page.info())
+//   .then(console.log); // Bruce Wayne
+
+// wiki()
+//   .page("COVID-19")
+//   .then((page) => page.info())
+//   .then(console.log);
+
+wiki()
+  .page("Future_Islands_discography")
+  .then((page) => page.info())
+  .then(console.log);
+
+// wiki().page("Future_Islands_discography").then(console.log);
